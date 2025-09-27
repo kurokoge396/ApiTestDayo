30d662c543f4a3cddcf1ff36bb99eb319ea369fc
kurokoge396 <396robato@gmail.com>
2025-09-27 14:18:41 +0900

    CSVファイルアップロードをとりあえず実装した

--- WebApplication2/Controllers/TestController.cs
+++ WebApplication2/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using WebApplication2.Interface;
 
 namespace WebApplication2.Controllers
@@ -32,6 +33,66 @@ namespace WebApplication2.Controllers
         {
             var todo = _todoTest.AddTodo(item);
             return todo is null ? BadRequest() : CreatedAtAction(nameof(GetTodo), new { Id = todo.Id }, todo);
+        }
+
+        /// <summary>
+        /// CSVアップロードAPIとりあえず版
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [HttpPost("csv-multi")]
+        [RequestSizeLimit(10 * 1024 * 1024)] // 最大10MB
+        public async Task<IActionResult> UploadMultipleCsv(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+                return BadRequest("ファイルが指定されていません。");
+
+            var resultList = new List<object>();
+
+            foreach (var file in files)
+            {
+                // 拡張子チェック
+                var ext = Path.GetExtension(file.FileName);
+                if (!ext.Equals(".csv", StringComparison.OrdinalIgnoreCase))
+                    return BadRequest($"不正なファイル形式: {file.FileName}");
+
+                var records = new List<string[]>();
+
+                using (var stream = file.OpenReadStream())
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    string? line;
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        var columns = line.Split(',');
+                        for (int i = 0; i < columns.Length; i++)
+                        {
+                            columns[i] = SanitizeCell(columns[i]);
+                        }
+                        records.Add(columns);
+                    }
+                }
+
+                resultList.Add(new
+                {
+                    FileName = Path.GetFileName(file.FileName),
+                    RowCount = records.Count,
+                    Sample = records.Count > 0 ? records[0] : null
+                });
+            }
+
+            return Ok(resultList);
+        }
+
+        private string SanitizeCell(string value)
+        {
+            if (!string.IsNullOrEmpty(value) &&
+                (value.StartsWith("=") || value.StartsWith("+") ||
+                 value.StartsWith("-") || value.StartsWith("@")))
+            {
+                return "'" + value; // CSVインジェクション対策
+            }
+            return value;
         }
     }
 }
